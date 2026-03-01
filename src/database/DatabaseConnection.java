dc29627cd88213c68a6196999840761af1efb349
k154y <yveskwizera154@gmail.com>
2026-03-01 21:17:16 +0200

    working db

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -2,13 +2,13 @@
 package database;
 import java.sql.*;
 public class DatabaseConnection {
-    private static final String URL="";
-    private  static final  String User="";
-    private static final String Password="";
+    private static final String URL="jdbc:mysql://localhost:3306/school"; 
+    private  static final  String USER="root";
+    private static final String PASSWORD="1234";
     public static Connection getConnection(){
         
     try{
-        Connection conn=DriverManager.getConnection(URL,User,Password);
+        Connection conn=DriverManager.getConnection(URL,USER,PASSWORD);
         System.out.print("connected successfully!");
         return conn;
         }
--- src/studentmanagementsystem/StudentManagementSystem.java
+++ src/studentmanagementsystem/StudentManagementSystem.java
@@ -1,20 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package studentmanagementsystem;
 
-/**
- *
- * @author User
- */
+import database.DatabaseConnection;
+import java.sql.Connection;
 public class StudentManagementSystem {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+          Connection conn = DatabaseConnection.getConnection();
+
+        if(conn != null){
+            System.out.println("Database is working!");
+        }
+        else{
+            System.out.println("Database connection error!");
+        }
     }
     
 }
