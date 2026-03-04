eb5ff873db6f0b7d237f6f76b50ccf6d4f38ca3e
User <User@172.31.190.65>
2026-03-04 22:22:04 +0200

    merging pages

--- src/studentmanagementsystem/StudentManagementSystem.java
+++ src/studentmanagementsystem/StudentManagementSystem.java
@@ -1,11 +1,4 @@
 
-<<<<<<< HEAD
-  /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-=======
->>>>>>> origin/backend
 package studentmanagementsystem;
 
 import database.DatabaseConnection;
