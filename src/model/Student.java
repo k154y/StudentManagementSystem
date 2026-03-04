6afd7d3e00429d230adfc5cb1064e9fe679dd5eb
k154y <yveskwizera154@gmail.com>
2026-03-04 20:34:43 +0200

    student and user updated

--- src/model/Student.java
+++ src/model/Student.java
@@ -2,6 +2,8 @@
 package model;
 import java.util.Scanner;
 import database.DatabaseOperations;
+import database.DatabaseConnection;
+import java.sql.*;
 
 public class Student extends Person implements DatabaseOperations  {
     private int id;
@@ -23,20 +25,83 @@ public class Student extends Person implements DatabaseOperations  {
     }
     @Override
     public void add(){
-        System.out.print("Enter name and Email");
-        email=sc.nextLine();
-        name=sc.nextLine(); 
-    }
+        try{
+            Connection conn=DatabaseConnection.getConnection();
+            String sql="INSERT INTO students(name,email,course,marks) VALUES(?,?,?,?)";
+            PreparedStatement ps=conn.prepareStatement(sql);
+            ps.setString(1,name);
+            ps.setString(2,email);
+            ps.setString(3,course);
+            ps.setDouble(4,marks);
+            ps.executeUpdate();
+            conn.close();
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+    }}
     @Override
     public void delete(){
-         System.out.print("Enter name and Email");
+        try{
+        Connection conn=database.DatabaseConnection.getConnection();
+        String sql="DELETE FROM students WHERE id=? ";
+        PreparedStatement ps=conn.prepareStatement(sql);
+        ps.setInt(1, id);
+        ps.executeUpdate();
+        conn.close();
+          }
+        catch(SQLException e){
+            e.printStackTrace();
+            System.out.println("Deleted Succesfully");
+        }
     }
     @Override
     public void update(){
-         System.out.print("Enter name and Email");
+          String sql = "UPDATE students SET name=?, email=?, course=?, marks=? WHERE id=?";
+
+    try (Connection conn = DatabaseConnection.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql)) {
+
+        ps.setString(1, name);
+        ps.setString(2, email);
+        ps.setString(3, course);
+        ps.setDouble(4, marks);
+        ps.setInt(5, id);
+
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+        
+    }
+         
     }
     @Override
     public void search(String keyword){
-         System.out.print("Enter name and Email"+keyword);
+         
+    String sql = "SELECT * FROM students WHERE name LIKE ? OR email LIKE ? OR course LIKE ?";
+
+    try (Connection conn = DatabaseConnection.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql)) {
+
+        String searchValue = "%" + keyword + "%";
+
+        ps.setString(1, searchValue);
+        ps.setString(2, searchValue);
+        ps.setString(3, searchValue);
+
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+
+            System.out.println("ID: " + rs.getInt("id"));
+            System.out.println("Name: " + rs.getString("name"));
+            System.out.println("Email: " + rs.getString("email"));
+            System.out.println("Course: " + rs.getString("course"));
+            System.out.println("Marks: " + rs.getDouble("marks"));
+            System.out.println("------------------------");
+        }
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
     }
 }
--- src/model/user.java
+++ src/model/user.java
@@ -8,6 +8,20 @@ package model;
  *
  * @author k.yves
  */
-public class user {
-    
+public class user 
+{
+    private int id;
+    private String password;
+    private String username;
+    private String  email;
+    public user(int id,String password,String username,String email){
+        this.id=id;
+        this.password=password;
+        this.username=username;
+        this.email=email;
+    }
+    public int getId() { return id; }
+    public String getUsername() { return username; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
 }
